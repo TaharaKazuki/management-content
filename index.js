522da6076702d35d404a9283d79b4a96fddea936
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-23 13:08:32 +0900

    update

--- index.js
+++ index.js
@@ -20,3 +20,65 @@ app.get('/api/resources/:id', (req, res) => {
   const resource = resources.find((resource) => resource.id === id)
   res.send(resource)
 })
+
+app.patch('/api/resources/:id', (req, res) => {
+  const resources = getResources()
+  const { id } = req.params
+  const index = resources.findIndex((resource) => resource.id === id)
+  const activeResource = resources.find(
+    (resource) => resource.status === 'active'
+  )
+
+  if (resources[index].status === 'complete') {
+    return res
+      .status(422)
+      .send('Cannot update because resource has been completed')
+  }
+
+  resources[index] = req.body
+
+  if (req.body.status === 'active') {
+    if (activeResource) {
+      return res.status(422).send('There is active resource already')
+    }
+
+    resources[index].status = 'active'
+    resources[index].activationTime = new Date()
+  }
+
+  fs.writeFileSync(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send('Cannot store data in the file')
+    }
+    return res.send('Data has been updated')
+  })
+})
+
+app.get('/api/activeresource', (req, res) => {
+  const resources = getResources()
+  const activeResource = resources.find(
+    (resource) => resource.status === 'active'
+  )
+  res.send(activeResource)
+})
+
+app.post('/api/resorces', (req, res) => {
+  const resources = getResources()
+  const resource = req.body
+
+  resource.createdAt = new Date()
+  resource.status = 'inactive'
+  resource.id = Date.now().toString()
+  resources.unshift(resource)
+
+  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+    if (error) {
+      return res.status(422).send('Cannot store data in the file')
+    }
+    return res.send('Data has been saved')
+  })
+})
+
+app.listen(PORT, () => {
+  console.info(`Server is Listening on PORT ${PORT}`)
+})
